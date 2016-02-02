f3801b1a2a18adc6f1ebf7f4d2e64363d6a1b078
Marcelo J. Both <marcelo.both@gmail.com>
2016-02-02 14:11:26 -0200

    Cria a remessa do Sicredi

---
+++ lib/brcobranca/remessa/cnab240/sicredi.rb
@@ -0,0 +1,183 @@
+# -*- encoding: utf-8 -*-
+module Brcobranca
+  module Remessa
+    module Cnab240
+      class Sicredi < Brcobranca::Remessa::Cnab240::Base
+
+        attr_accessor :modalidade_carteira
+        # identificacao da emissao do boleto (attr na classe base)
+        #   opcoes:
+        #     ‘1’ = Banco Emite
+        #     ‘2’ = Cliente Emite
+        #
+        # identificacao da distribuicao do boleto (attr na classe base)
+        #   opcoes:
+        #     ‘1’ = Banco distribui
+        #     ‘2’ = Cliente distribui
+
+        attr_accessor :tipo_formulario
+        #       Tipo Formulário - 01 posição  (15 a 15):
+        #            "1" -auto-copiativo
+        #            "3" -auto-envelopável
+        #            "4" -A4 sem envelopamento
+        #            "6" -A4 sem envelopamento 3 vias
+
+        attr_accessor :parcela
+        #       Parcela - 02 posições (11 a 12) - "01" se parcela única
+
+        validates_presence_of :modalidade_carteira, :tipo_formulario, :parcela, message: 'não pode estar em branco.'
+        # Remessa 400 - 8 digitos
+        # Remessa 240 - 12 digitos
+        validates_length_of :conta_corrente, maximum: 8, message: 'deve ter 8 dígitos.'
+        validates_length_of :agencia, is: 4, message: 'deve ter 4 dígitos.'
+        validates_length_of :modalidade_carteira, is: 2, message: 'deve ter 2 dígitos.'
+
+        def initialize(campos = {})
+          campos = { emissao_boleto: '2',
+            distribuicao_boleto: '2',
+            especie_titulo: '02',
+            tipo_formulario: '4',
+            parcela: '01',
+            modalidade_carteira: '01',
+            forma_cadastramento: '0'}.merge!(campos)
+          super(campos)
+        end
+
+        def cod_banco
+          '756'
+        end
+
+        def nome_banco
+          'SICOOB'.ljust(30, ' ')
+        end
+
+        def versao_layout_arquivo
+          '081'
+        end
+
+        def versao_layout_lote
+          '040'
+        end
+
+        def digito_agencia
+          # utilizando a agencia com 4 digitos
+          # para calcular o digito
+          agencia.modulo11(mapeamento: { 10 => 'X' }).to_s
+        end
+
+        def digito_conta
+          # utilizando a conta corrente com 5 digitos
+          # para calcular o digito
+          conta_corrente.modulo11(mapeamento: { 10 => 'X' }).to_s
+        end
+
+        def codigo_convenio
+          # CAMPO                TAMANHO
+          # num. convenio        20 BRANCOS
+          ''.rjust(20, ' ')
+        end
+
+        alias_method :convenio_lote, :codigo_convenio
+
+        def info_conta
+          # CAMPO                  TAMANHO
+          # agencia                5
+          # digito agencia         1
+          # conta corrente         12
+          # digito conta           1
+          # digito agencia/conta   1
+          "#{agencia.rjust(5, '0')}#{digito_agencia}#{conta_corrente.rjust(12, '0')}#{digito_conta} "
+        end
+
+        def complemento_header
+          ''.rjust(29, ' ')
+        end
+
+        def quantidade_titulos_cobranca
+          pagamentos.length.to_s.rjust(6, "0")
+        end
+
+        def totaliza_valor_titulos
+          pagamentos.inject(0) { |sum, pag| sum += pag.valor.to_f }
+        end
+
+        def valor_titulos_carteira
+          total = sprintf "%.2f", totaliza_valor_titulos
+          total.somente_numeros.rjust(17, "0")
+        end
+
+        def complemento_trailer
+          # CAMPO                               TAMANHO
+          # Qt. Títulos em Cobrança Simples     6
+          # Vl. Títulos em Carteira Simples     15 + 2 decimais
+          # Qt. Títulos em Cobrança Vinculada   6
+          # Vl. Títulos em Carteira Vinculada   15 + 2 decimais
+          # Qt. Títulos em Cobrança Caucionada  6
+          # Vl. Títulos em Carteira Caucionada  15 + 2 decimais
+          # Qt. Títulos em Cobrança Descontada  6
+          # Vl. Títulos em Carteira Descontada  15 + 2 decimais
+          total_cobranca_simples    = "#{quantidade_titulos_cobranca}#{valor_titulos_carteira}"
+          total_cobranca_vinculada  = "".rjust(23, "0")
+          total_cobranca_caucionada = "".rjust(23, "0")
+          total_cobranca_descontada = "".rjust(23, "0")
+
+          "#{total_cobranca_simples}#{total_cobranca_vinculada}#{total_cobranca_caucionada}"\
+            "#{total_cobranca_descontada}".ljust(217, ' ')
+        end
+
+        # Monta o registro trailer do arquivo
+        #
+        # @param nro_lotes [Integer]
+        #   numero de lotes no arquivo
+        # @param sequencial [Integer]
+        #   numero de registros(linhas) no arquivo
+        #
+        # @return [String]
+        #
+        def monta_trailer_arquivo(nro_lotes, sequencial)
+          # CAMPO                     TAMANHO
+          # codigo banco              3
+          # lote de servico           4
+          # tipo de registro          1
+          # uso FEBRABAN              9
+          # nro de lotes              6
+          # nro de registros(linhas)  6
+          # uso FEBRABAN              211
+          "#{cod_banco}99999#{''.rjust(9, ' ')}#{nro_lotes.to_s.rjust(6, '0')}#{sequencial.to_s.rjust(6, '0')}#{''.rjust(6, '0')}#{''.rjust(205, ' ')}"
+        end
+
+        def codigo_protesto
+          "1"
+        end
+
+        def complemento_p(pagamento)
+          # CAMPO                   TAMANHO
+          # conta corrente          12
+          # digito conta            1
+          # digito agencia/conta    1
+          # ident. titulo no banco  20
+          "#{conta_corrente.rjust(12, '0')}#{digito_conta} #{formata_nosso_numero(pagamento.nosso_numero)}"
+        end
+
+        # Retorna o nosso numero
+        #
+        # @return [String]
+        #
+        # Nosso Número:
+        #  - Se emissão a cargo do Cedente (vide planilha "Capa" deste arquivo):
+        #       NumTitulo - 10 posições (1 a 10)
+        #       Parcela - 02 posições (11 a 12) - "01" se parcela única
+        #       Modalidade - 02 posições (13 a 14) - vide planilha "Capa" deste arquivo
+        #       Tipo Formulário - 01 posição  (15 a 15):
+        #            "1" -auto-copiativo
+        #            "3" -auto-envelopável
+        #            "4" -A4 sem envelopamento
+        #            "6" -A4 sem envelopamento 3 vias
+        #       Em branco - 05 posições (16 a 20)
+        def formata_nosso_numero(nosso_numero)
+          "#{nosso_numero.to_s.rjust(10, '0')}#{parcela}#{modalidade_carteira}#{tipo_formulario}     "
+        end
+      end
+    end
+  end
+end
---
+++ spec/brcobranca/remessa/cnab240/sicredi_spec.rb
@@ -0,0 +1,161 @@
+# -*- encoding: utf-8 -*-
+require 'spec_helper'
+
+RSpec.describe Brcobranca::Remessa::Cnab240::Sicredi do
+  let(:pagamento) do
+    Brcobranca::Remessa::Pagamento.new(
+      valor: 50.0,
+      data_vencimento: Date.today,
+      nosso_numero: '429715',
+      documento_sacado: '82136760505',
+      nome_sacado: 'PABLO DIEGO JOSÉ FRANCISCO DE PAULA JUAN NEPOMUCENO MARÍA DE LOS REMEDIOS CIPRIANO DE LA SANTÍSSIMA TRINIDAD RUIZ Y PICASSO',
+      endereco_sacado: 'RUA RIO GRANDE DO SUL São paulo Minas caçapa da silva junior',
+      bairro_sacado: 'São josé dos quatro apostolos magros',
+      cep_sacado: '12345678',
+      cidade_sacado: 'Santa rita de cássia maria da silva',
+      uf_sacado: 'RJ'
+    )
+  end
+
+  let(:params) do
+    {
+      empresa_mae: 'SOCIEDADE BRASILEIRA DE ZOOLOGIA LTDA',
+      agencia: '4327',
+      conta_corrente: '03666',
+      documento_cedente: '74576177000177',
+      modalidade_carteira: '01',
+      pagamentos: [pagamento]
+    }
+  end
+
+  let(:sicredi) { subject.class.new(params) }
+
+  context 'validacoes' do
+    context '@modalidade_carteira' do
+      it 'deve ser invalido se nao possuir a modalidade da carteira' do
+        objeto = subject.class.new(params.merge(modalidade_carteira: nil))
+        expect(objeto.invalid?).to be true
+        expect(objeto.errors.full_messages).to include('Modalidade carteira não pode estar em branco.')
+      end
+    end
+
+    context '@tipo_formulario' do
+      it 'deve ser invalido se nao possuir o tipo de formulario' do
+        objeto = subject.class.new(params.merge(tipo_formulario: nil))
+        expect(objeto.invalid?).to be true
+        expect(objeto.errors.full_messages).to include('Tipo formulario não pode estar em branco.')
+      end
+    end
+
+    context '@parcela' do
+      it 'deve ser invalido se nao possuir a parcela' do
+        objeto = subject.class.new(params.merge(parcela: nil))
+        expect(objeto.invalid?).to be true
+        expect(objeto.errors.full_messages).to include('Parcela não pode estar em branco.')
+      end
+    end
+
+    context '@agencia' do
+      it 'deve ser invalido se a agencia tiver mais de 4 digitos' do
+        sicredi.agencia = '12345'
+        expect(sicredi.invalid?).to be true
+        expect(sicredi.errors.full_messages).to include('Agencia deve ter 4 dígitos.')
+      end
+    end
+
+    context '@conta_corrente' do
+      it 'deve ser invalido se a conta corrente tiver mais de 8 digitos' do
+        sicredi.conta_corrente = '123456789'
+        expect(sicredi.invalid?).to be true
+        expect(sicredi.errors.full_messages).to include('Conta corrente deve ter 8 dígitos.')
+      end
+    end
+  end
+
+  context 'formatacoes' do
+    it 'codigo do banco deve ser 001' do
+      expect(sicredi.cod_banco).to eq '756'
+    end
+
+    it 'nome do banco deve ser sicredi com 30 posicoes' do
+      nome_banco = sicredi.nome_banco
+      expect(nome_banco.size).to eq 30
+      expect(nome_banco[0..19]).to eq 'sicredi              '
+    end
+
+    it 'versao do layout do arquivo deve ser 081' do
+      expect(sicredi.versao_layout_arquivo).to eq '081'
+    end
+
+    it 'versao do layout do lote deve ser 040' do
+      expect(sicredi.versao_layout_lote).to eq '040'
+    end
+
+    it 'deve calcular o digito da agencia' do
+      # digito calculado a partir do modulo 11 com base 9
+      #
+      # agencia = 1  2  3  4
+      #
+      #           4  3  2  1
+      # x         9  8  7  6
+      # =         36 24 14 6 = 80
+      # 80 / 11 = 7 com resto 3
+      expect(sicredi.digito_agencia).to eq '3'
+    end
+
+    it 'deve calcular  digito da conta' do
+      # digito calculado a partir do modulo 11 com base 9
+      #
+      # conta = 1  2  3  4  5
+      #
+      #         5  4  3  2  1
+      # x       9  8  7  6  5
+      # =       45 32 21 12 5 = 116
+      # 116 / 11 = 10 com resto 5
+      expect(sicredi.digito_conta).to eq '8'
+    end
+
+    it 'cod. convenio deve retornar as informacoes nas posicoes corretas' do
+      cod_convenio = sicredi.codigo_convenio
+      expect(cod_convenio[0..19]).to eq '                    '
+    end
+
+    it 'info conta deve retornar as informacoes nas posicoes corretas' do
+      info_conta = sicredi.info_conta
+      expect(info_conta[0..4]).to eq '04327'
+      expect(info_conta[5]).to eq '3'
+      expect(info_conta[6..17]).to eq '000000003666'
+      expect(info_conta[18]).to eq '8'
+    end
+
+    it 'complemento header deve retornar espacos em branco' do
+      expect(sicredi.complemento_header).to eq ''.rjust(29, ' ')
+    end
+
+    it 'complemento trailer deve retornar espacos em branco com a totalização das cobranças' do
+      total_cobranca_simples    = "00000100000000000005000"
+      total_cobranca_vinculada  = "".rjust(23, "0")
+      total_cobranca_caucionada = "".rjust(23, "0")
+      total_cobranca_descontada = "".rjust(23, "0")
+
+      expect(sicredi.complemento_trailer).to eq "#{total_cobranca_simples}#{total_cobranca_vinculada}"\
+                            "#{total_cobranca_caucionada}#{total_cobranca_descontada}".ljust(217, ' ')
+    end
+
+    it 'formata o nosso numero' do
+      nosso_numero = sicredi.formata_nosso_numero 1
+      expect(nosso_numero).to eq "000000000101014     "
+    end
+  end
+
+  context 'geracao remessa' do
+    it_behaves_like 'cnab240'
+
+    context 'arquivo' do
+      before { Timecop.freeze(Time.local(2015, 7, 14, 16, 15, 15)) }
+      after { Timecop.return }
+
+      it { expect(sicredi.gera_arquivo).to eq(read_remessa('remessa-bancoob-cnab240.rem', sicoob.gera_arquivo)) }
+    end
+  end
+end
