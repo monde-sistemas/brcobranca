ea1e2fd2610fba19d04d2409dec799856785a835
Marcelo J. Both <marcelo.both@gmail.com>
2016-11-28 17:29:39 -0200

    Corrige formatação do nosso número do Sicoob no boleto bancário (#98)
    * Formata nosso número.

--- lib/brcobranca/boleto/sicoob.rb
+++ lib/brcobranca/boleto/sicoob.rb
@@ -60,7 +60,7 @@ module Brcobranca
       #
       # @return [String] 8 caracteres numéricos.
       def nosso_numero_boleto
-        "#{numero}#{nosso_numero_dv}"
+        "#{numero}-#{nosso_numero_dv}"
       end
 
       # 3.13. Nosso número: Código de controle que permite ao Sicoob e à empresa identificar os dados da cobrança que deu origem ao boleto.
@@ -111,7 +111,7 @@ module Brcobranca
       #    34 a 41      08                 Nosso número do boleto
       #    41 a 44      03                 Número da parcela a que o boleto se refere - "001" se parcela única
       def codigo_barras_segunda_parte
-        "#{carteira}#{agencia}#{variacao}#{convenio}#{nosso_numero_boleto}#{quantidade}"
+        "#{carteira}#{agencia}#{variacao}#{convenio}#{numero}#{nosso_numero_dv}#{quantidade}"
       end
     end
   end
