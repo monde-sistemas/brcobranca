38d5ad0d4fa003a3e1fd8a11c7eedd9d4003a82d
Marcelo J. Both <marcelo.both@gmail.com>
2017-02-23 17:18:39 -0300

    Renomeia variáveis"

--- spec/brcobranca/remessa/cnab240/unicred_spec.rb
+++ spec/brcobranca/remessa/cnab240/unicred_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Brcobranca::Remessa::Cnab240::Unicred do
     }
   end
 
-  let(:sicredi) { subject.class.new(params) }
+  let(:unicred) { subject.class.new(params) }
 
   before { Timecop.freeze(Time.local(2007, 7, 14, 16, 15, 15)) }
   after { Timecop.return }
@@ -47,9 +47,9 @@ RSpec.describe Brcobranca::Remessa::Cnab240::Unicred do
       end
 
       it 'deve ser invalido se o posto tiver mais de 2 dígitos' do
-        sicredi.posto = '123'
-        expect(sicredi.invalid?).to be true
-        expect(sicredi.errors.full_messages).to include('Posto é muito longo (máximo: 2 caracteres).')
+        unicred.posto = '123'
+        expect(unicred.invalid?).to be true
+        expect(unicred.errors.full_messages).to include('Posto é muito longo (máximo: 2 caracteres).')
       end
     end
 
@@ -61,9 +61,9 @@ RSpec.describe Brcobranca::Remessa::Cnab240::Unicred do
       end
 
       it 'deve ser invalido se o byte idt tiver mais de 1 dígito' do
-        sicredi.byte_idt = '12'
-        expect(sicredi.invalid?).to be true
-        expect(sicredi.errors.full_messages)
+        unicred.byte_idt = '12'
+        expect(unicred.invalid?).to be true
+        expect(unicred.errors.full_messages)
           .to include('Byte idt deve ser 1 se o numero foi gerado pela agencia ou 2-9 se foi gerado pelo beneficiário')
       end
     end
@@ -86,56 +86,56 @@ RSpec.describe Brcobranca::Remessa::Cnab240::Unicred do
 
     context '@agencia' do
       it 'deve ser invalido se a agencia tiver mais de 4 digitos' do
-        sicredi.agencia = '12345'
-        expect(sicredi.invalid?).to be true
-        expect(sicredi.errors.full_messages).to include('Agencia deve ter 4 dígitos.')
+        unicred.agencia = '12345'
+        expect(unicred.invalid?).to be true
+        expect(unicred.errors.full_messages).to include('Agencia deve ter 4 dígitos.')
       end
     end
 
     context '@conta_corrente' do
       it 'deve ser invalido se a conta corrente tiver mais de 5 digitos' do
-        sicredi.conta_corrente = '123456'
-        expect(sicredi.invalid?).to be true
-        expect(sicredi.errors.full_messages).to include('Conta corrente é muito longo (máximo: 5 caracteres).')
+        unicred.conta_corrente = '123456'
+        expect(unicred.invalid?).to be true
+        expect(unicred.errors.full_messages).to include('Conta corrente é muito longo (máximo: 5 caracteres).')
       end
     end
 
     context '@digito_conta' do
       it 'deve ser inválido se o dígito conta não for informado' do
-        sicredi.digito_conta = nil
-        expect(sicredi.invalid?).to be true
-        expect(sicredi.errors.full_messages).to include('Digito conta não pode estar em branco.')
+        unicred.digito_conta = nil
+        expect(unicred.invalid?).to be true
+        expect(unicred.errors.full_messages).to include('Digito conta não pode estar em branco.')
       end
 
       it 'deve ser inválido se o dígito conta tiver mais de 1 dígito' do
-        sicredi.digito_conta = '12'
-        expect(sicredi.invalid?).to be true
-        expect(sicredi.errors.full_messages).to include('Digito conta deve ter 1 dígito.')
+        unicred.digito_conta = '12'
+        expect(unicred.invalid?).to be true
+        expect(unicred.errors.full_messages).to include('Digito conta deve ter 1 dígito.')
       end
     end
   end
 
   context 'formatacoes' do
     it 'codigo do banco deve ser 001' do
-      expect(sicredi.cod_banco).to eq '748'
+      expect(unicred.cod_banco).to eq '748'
     end
 
-    it 'nome do banco deve ser sicredi com 30 posicoes' do
-      nome_banco = sicredi.nome_banco
+    it 'nome do banco deve ser unicred com 30 posicoes' do
+      nome_banco = unicred.nome_banco
       expect(nome_banco.size).to eq 30
       expect(nome_banco[0..19]).to eq 'SICREDI             '
     end
 
     it 'versao do layout do arquivo deve ser 081' do
-      expect(sicredi.versao_layout_arquivo).to eq '081'
+      expect(unicred.versao_layout_arquivo).to eq '081'
     end
 
     it 'versao do layout do lote deve ser 040' do
-      expect(sicredi.versao_layout_lote).to eq '040'
+      expect(unicred.versao_layout_lote).to eq '040'
     end
 
     it 'deve calcular o digito da agencia' do
-      expect(sicredi.digito_agencia).to eq ' '
+      expect(unicred.digito_agencia).to eq ' '
     end
 
     it 'deve calcular  digito da conta' do
@@ -147,16 +147,16 @@ RSpec.describe Brcobranca::Remessa::Cnab240::Unicred do
       # x       9  8  7  6  5
       # =       45 32 21 12 5 = 116
       # 116 / 11 = 10 com resto 5
-      expect(sicredi.digito_conta).to eq '8'
+      expect(unicred.digito_conta).to eq '8'
     end
 
     it 'cod. convenio deve retornar as informacoes nas posicoes corretas' do
-      cod_convenio = sicredi.codigo_convenio
+      cod_convenio = unicred.codigo_convenio
       expect(cod_convenio[0..19]).to eq ''.rjust(20, ' ')
     end
 
     it 'info conta deve retornar as informacoes nas posicoes corretas' do
-      info_conta = sicredi.info_conta
+      info_conta = unicred.info_conta
       expect(info_conta[0..4]).to eq '00165'
       expect(info_conta[5]).to eq ' '
       expect(info_conta[6..17]).to eq '000000000623'
@@ -164,7 +164,7 @@ RSpec.describe Brcobranca::Remessa::Cnab240::Unicred do
     end
 
     it 'complemento header deve retornar espacos em branco' do
-      expect(sicredi.complemento_header).to eq ''.rjust(29, ' ')
+      expect(unicred.complemento_header).to eq ''.rjust(29, ' ')
     end
 
     it 'complemento trailer deve retornar espacos em branco com a totalização das cobranças' do
@@ -173,12 +173,12 @@ RSpec.describe Brcobranca::Remessa::Cnab240::Unicred do
       total_cobranca_caucionada = "".rjust(23, "0")
       total_cobranca_descontada = "".rjust(23, "0")
 
-      expect(sicredi.complemento_trailer).to eq "#{total_cobranca_simples}#{total_cobranca_vinculada}"\
+      expect(unicred.complemento_trailer).to eq "#{total_cobranca_simples}#{total_cobranca_vinculada}"\
         "#{total_cobranca_caucionada}#{total_cobranca_descontada}".ljust(217, ' ')
     end
 
     it 'formata o nosso numero' do
-      nosso_numero = sicredi.formata_nosso_numero "072000031"
+      nosso_numero = unicred.formata_nosso_numero "072000031"
       expect(nosso_numero.strip).to eq "072000031"
     end
   end
@@ -187,7 +187,7 @@ RSpec.describe Brcobranca::Remessa::Cnab240::Unicred do
     it_behaves_like 'cnab240'
 
     context 'arquivo' do
-      it { expect(sicredi.gera_arquivo).to eq(read_remessa('remessa-sicredi-cnab240.rem', sicredi.gera_arquivo)) }
+      it { expect(unicred.gera_arquivo).to eq(read_remessa('remessa-unicred-cnab240.rem', unicred.gera_arquivo)) }
     end
   end
 end
