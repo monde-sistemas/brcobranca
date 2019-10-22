73b17f1607556d67f799b1cf1a0f54fb59a9103e
Anderson Macedo <anderson.krs95@gmail.com>
2019-10-22 11:53:10 -0700

    Remove linha em branco

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
-
     with.library :active_model
   end
 end
